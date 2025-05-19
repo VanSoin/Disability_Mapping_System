ad4220d5ddd34fedbb36f86ca0320ca1fbbfe645
VanSoin <vanshikasoin2@gmail.com>
2025-05-19 18:12:26 +0530

    Create layout.jsx

---
+++ layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import { SidebarItem } from "./Sidebar";
+import { Outlet } from "react-router-dom";
+import { LayoutDashboard } from "lucide-react";
+
+export default function Layout() {
+  return (
+    <div className="flex h-screen bg-gradient-to-r from-[#d4c084] to-[#e60202]">
+      <Sidebar>
+        <SidebarItem
+          icon={<LayoutDashboard size={20} />}
+          text="Location"
+          path="/upload"
+          active
+        />
+      </Sidebar>
+      <div className="flex-grow bg-gradient-to-r from-[#d4c084] to-[#e60202] p-4">
+        <Outlet /> {/* This renders the active page content */}
+      </div>
+    </div>
+  );
+}
